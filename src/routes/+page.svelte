1fe9e6ad50c9cf5438eea889f73373c692c26b2c
Dustin_dusTir <du-wal@web.de>
2022-10-31 01:10:05 +0100

    setInterval from Tutorial
    https://svelte.dev/tutorial/ondestroy

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
+<script>
+  import { onDestroy } from "svelte";
+
+  let counter = 0;
+  const interval = setInterval(() => (counter += 1), 1000);
+
+  onDestroy(() => clearInterval(interval));
+</script>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
