59d5497b7821690bc24e44b08b4a3c31f8308f61
Dustin_dusTir <du-wal@web.de>
2022-10-31 01:29:03 +0100

    adapter-static
    https://github.com/sveltejs/kit/tree/master/packages/adapter-static

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,16 @@
-import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter({
-			// if true, will create a Netlify Edge Function rather
-			// than using standard Node-based functions
-			edge: false,
-	  
-			// if true, will split your app into multiple functions
-			// instead of creating a single one for the entire app.
-			// if `edge` is true, this option cannot be used
-			split: false
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false,
+			strict: true
 		  })
 	}
 };
